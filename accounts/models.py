8b68035cae2a75b70fbb1d4f500bb04bf98df349
FaithCoderLab <seinplan97@gmail.com>
2025-01-31 11:19:07 +0900

    프로필 사진 기능 추가

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=50)
+    profile_image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
